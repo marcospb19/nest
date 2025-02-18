ffc5169703a66eaf8f3cb7494c7e86a88410ab51
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2025-02-18 20:28:22 -0300

    fix: define 'update_done_state' return value as Option

--- src/app.rs
+++ src/app.rs
@@ -148,10 +148,11 @@ impl App<'_> {
         }
     }
 
-    pub fn update_done_state(&mut self) {
-        let selected_task = self.get_selected_task().unwrap();
+    pub fn update_done_state(&mut self) -> Option<()> {
+        let selected_task = self.get_selected_task()?;
         let new_done_state = !selected_task.done;
         self.storage.update_task_state(selected_task.id, new_done_state);
+        Some(())
     }
 
     pub fn get_back_to_parent(&mut self) -> Option<()> {
